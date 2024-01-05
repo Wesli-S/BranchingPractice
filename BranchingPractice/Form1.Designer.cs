83ba28eac8cd16c6704783dd5fd739165b519d0e
Wesli-S <146999017+Wesli-S@users.noreply.github.com>
2024-01-05 11:05:29 -0800

    Added greeting for when user submits their name

--- BranchingPractice/Form1.Designer.cs
+++ BranchingPractice/Form1.Designer.cs
@@ -28,18 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnDisplayGreeting = new Button();
+            txtName = new TextBox();
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // btnDisplayGreeting
+            // 
+            btnDisplayGreeting.Location = new Point(40, 108);
+            btnDisplayGreeting.Name = "btnDisplayGreeting";
+            btnDisplayGreeting.Size = new Size(120, 57);
+            btnDisplayGreeting.TabIndex = 0;
+            btnDisplayGreeting.Text = "Display Greeting";
+            btnDisplayGreeting.UseVisualStyleBackColor = true;
+            btnDisplayGreeting.Click += btnDisplayGreeting_Click;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(12, 45);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(187, 23);
+            txtName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(49, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(98, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Write your name!";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(211, 200);
+            Controls.Add(label1);
+            Controls.Add(txtName);
+            Controls.Add(btnDisplayGreeting);
             Name = "Form1";
             Text = "Branching Practice";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnDisplayGreeting;
+        private TextBox txtName;
+        private Label label1;
     }
 }
